d076d2a86106545c1ef8c00b89331bdc0d499511
clarenceagcc <agccclarence@gmail.com>
2023-10-05 13:41:23 +0800

    lab4 stuff pt2

--- Lab4/elfish.py
+++ Lab4/elfish.py
@@ -3,11 +3,14 @@ import sys
 
 def elfish(str):
     elfish_words = 'elf'
+    count = 0
+    for i in elfish_words:
+        if i in str.lower():
+            count += 1
 
-    if elfish_words in str.lower():
+    if count == len(elfish_words):
         return f'{str} is one elfish word!'
     else:
         return f'{str} is not an elfish word!'
-    
 
 print(elfish(sys.argv[1]))
