ccb0b1f15e43a72c81c25e3724b8286d972c6c0a
clarenceagcc <agccclarence@gmail.com>
2023-10-31 21:25:49 +0800

    c proj

--- C_test_folder/c_projectTest.c
+++ C_test_folder/c_projectTest.c
@@ -1,15 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+// hashtable item
+typedef struct Ht_item
+{
+    char* key;
+    char* value;
+} Ht_item;
+
+// Defining hash table
+typedef struct HashTable
+{
+    // Contains an array of pointers to items.
+    Ht_item** items;
+    int size;
+    int count;
+} HashTable;
 
 // Prototyping
 bool openFile(char *pFileName);
+void printTable(HashTable* table);
+Ht_item* create_item(char* key, char* value);
+HashTable* create_table(int size);
+void ht_insert(HashTable* table, char* key, char* value);
+unsigned long hash_function(char* str);
+void ht_delete(HashTable *table, char *key);
+
+// Constants
+#define CAPACITY 50000 // size of Hashtable
+
 
 int main()
 {   
     char pFileName[20];
     char command[20];
     bool ifExited = false;
+    HashTable *ht = create_table(CAPACITY);
+    printf("TESTING FIRST WITHOUT DELETING\n");
+    ht_insert(ht, (char *)"1", (char *)"test1");
+    ht_insert(ht, (char *)"2", (char *)"test2");
+    printTable(ht);
+    printf("----------------------------------\n");
+    printf("TESTING AFTER DELETING\n");
+    ht_delete(ht, (char *)"1");
+    printTable(ht);
 
     while (!ifExited)
     {
@@ -24,6 +60,77 @@ int main()
     }
     return 0;
 }
+void ht_insert(HashTable* table, char* key, char* value)
+{
+    // Creates the item.
+    Ht_item *item = create_item(key, value);
+    // Computes the index.
+    int index = hash_function(key);
+    Ht_item *current_item = table->items[index];
+    if (current_item == NULL)
+    {
+        // Insert directly.
+        table->items[index] = item;
+        table->count++;
+    }
+}
+void ht_delete(HashTable *table, char *key)
+{
+    int index = hash_function(key);
+    Ht_item *item = table->items[index];
+    if (item != NULL && strcmp(item->key, key) == 0)
+    {
+        table->items[index] = NULL;
+        table->count--;
+    }
+}
+
+
+unsigned long hash_function(char* str)
+{
+    unsigned long i = 0;
+    for (int j = 0; str[j]; j++)
+        i += str[j];
+    return i % CAPACITY;
+}
+
+Ht_item* create_item(char* key, char* value)
+{
+    // Creates a pointer to a new HashTable item.
+    Ht_item* item = (Ht_item*) malloc(sizeof(Ht_item));
+    item->key = (char*) malloc(strlen(key) + 1);
+    item->value = (char*) malloc(strlen(value) + 1);
+    strcpy(item->key, key);
+    strcpy(item->value, value);
+    return item;
+}
+
+HashTable* create_table(int size)
+{
+    // Creates a new HashTable.
+    HashTable* table = (HashTable*) malloc(sizeof(HashTable));
+    table->size = size;
+    table->count = 0;
+    table->items = (Ht_item**) calloc(table->size, sizeof(Ht_item*));
+    for (int i = 0; i < table->size; i++)
+        table->items[i] = NULL;
+    return table;
+}
+
+void printTable(HashTable* table)
+{
+    printf("\nHASH TABLE TEST\n");
+
+    for (int i = 0; i < table->size; i++)
+    {
+        if (table->items[i])
+        {
+            printf("Key:%s, Value:%s\n",table->items[i] -> key, table->items[i]->value);
+        }
+    }
+}
+
+
 
 bool openFile(char *pFileName) 
 {
--- Lab9/tinygrep.c
+++ Lab9/tinygrep.c
@@ -1,42 +1,68 @@
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
     char buffer[255];
     char pattern[255];
-    int caseSensitive = 1;
-    printf("Enter a sentence, up to 255 characters\n");
+    int caseSensitive = 1, pos = -1;
+    
+    printf("Enter a sentence, up to 255 characters.\n");
     fgets(buffer, 255, stdin);
     buffer[strcspn(buffer, "\n")] = '\0'; // remove newline from fgets
 
-    printf("Enter the pattern\n");
-    scanf("%s", &pattern);
+    printf("Enter the pattern.\n");
+    fgets(pattern, 255, stdin);
+    pattern[strcspn(pattern, "\n")] = '\0'; // remove newline from fgets
 
     printf("1 for case sensitive, 0 for no case sensitive\n");
-    scanf("%d", &caseSensitive);
+    scanf("%d", &caseSensitive); // storing input either 0 or 1
 
-    if (caseSensitive == 1)
-    {
-        // if input caseSensitive is 1 make everything to lowercase
-        for (int i = 0; buffer[i]; i++)
-        {
-            buffer[i] = tolower(buffer[i]);
+    if (caseSensitive == 0) {
+        // if input caseSensitive is 0 make everything to lowercase
+        for (int i = 0; buffer[i]; i++) {
+        buffer[i] = tolower(buffer[i]);
+        }
+        for (int i = 0; pattern[i]; i++) {
+        pattern[i] = tolower(pattern[i]);
+        }
+    } // case sensitive changer
+    // checking each char code block
+    //get length of strings
+    int mainStrLen = strlen(buffer);
+    int patternLen = strlen(pattern);
+
+    for (int i = 0; i <= mainStrLen - patternLen; i++) {
+        int x; // declaring x for storing position
+        for (x = 0; x < patternLen; x++) {
+        char mainStr = buffer[x + i];
+        char patternStr = pattern[x];
+
+        if (patternStr == '_') {
+            if (!isspace(patternStr)) {
+            break;
+            }
+        }
+        else if (patternStr == '.') { /* ignore this since '.' can be any char*/ } 
+        else if (patternStr != mainStr) {
+            break; // if pattern does not match break out of for loop
+        };
+        }
+        if (x == patternLen) {
+        pos = i; // returns first positon found and break out.
+        break;
         }
     }
-    char *position = strstr(buffer, pattern);
-
-    if (position != NULL) {
-        int pos = position - buffer;
-        printf("Matches at position %d.", pos);
+    // printing statements
+    if (pos != -1)
+    {
+        printf("Matches at position %d.\n", pos);
     }
-    else {
-        printf("No match.");
+    else
+    {
+        printf("No match.\n");
     }
-    
-    
     
 
     return 0;
